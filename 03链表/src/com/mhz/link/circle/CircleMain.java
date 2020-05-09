1d7ce295e3cf8b6a8aa32703256eed0f3350339a
mahaizhen <229358084@qq.com>
2020-05-09 18:09:24 +0800

    双向链表

--- 03链表/src/com/mhz/link/circle/CircleMain.java
+++ 03链表/src/com/mhz/link/circle/CircleMain.java
@@ -5,12 +5,20 @@ import com.mhz.link.List;
 public class CircleMain {
 
 	public static void main(String[] args) {
-		List<Integer> circleList = new CircleSingleLinkedList<Integer>();
+		textList(new CircleDoubleLinkedList<Integer>());
+
+	}
+
+	public static void textList(List<Integer> circleList) {
 		circleList.add(21);
 		circleList.add(56);
 		circleList.add(78);
 		circleList.add(85);
-		circleList.add(0, 69);
+//		circleList.add(1, 69);// [21-69-56 78 85]
 		circleList.toString();
+		
+		circleList.remove(0); // [69-56 78 85]   
+		circleList.toString();
+
 	}
 }
