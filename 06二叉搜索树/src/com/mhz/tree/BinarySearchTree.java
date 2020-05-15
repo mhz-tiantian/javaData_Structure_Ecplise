fdc20da42626a371e3837bee9913691631cb3957
mahaizhen <229358084@qq.com>
2020-05-15 18:21:26 +0800

    搜索二叉树, 前序, 中序, 遍历

--- 06二叉搜索树/src/com/mhz/tree/BinarySearchTree.java
+++ 06二叉搜索树/src/com/mhz/tree/BinarySearchTree.java
@@ -1,5 +1,11 @@
 package com.mhz.tree;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.mhz.tree.printer.BinaryTreeInfo;
+
 /**
  * 二叉树 搜索
  * 
@@ -7,7 +13,16 @@ package com.mhz.tree;
  *
  * @date 2020年5月14日
  */
-public class BinarySearchTree<E> {
+public class BinarySearchTree<E> implements BinaryTreeInfo {
+
+	private Comparator<E> comparator;
+
+	public BinarySearchTree() {
+	}
+
+	public BinarySearchTree(Comparator<E> comparator) {
+		this.comparator = comparator;
+	}
 
 	// 二叉树的根节点
 	private Node<E> root;
@@ -38,6 +53,189 @@ public class BinarySearchTree<E> {
 
 	}
 
+	public void levelOrder(Visitor<E> visitor) {
+		if (root == null || visitor == null) {
+			return;
+		}
+		// 生成一个队列 并且放回root
+		Queue<Node<E>> queue = new LinkedList<>();
+		// 往队列里,添加元素
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			// 从队头获取元素, 并且更新队列
+			Node<E> current = queue.poll();
+			boolean stop = visitor.visitor(current.element);
+			// stop 如果为true 表示的是遍历结束, 直接返回
+			if (stop) {
+				return;
+			}
+			Node<E> left = current.left;
+			if (left != null) {
+				queue.add(left);
+			}
+			Node<E> right = current.right;
+			if (right != null) {
+				queue.add(right);
+			}
+		}
+	}
+
+	/**
+	 * 层序遍历
+	 */
+	private void levelOrderTraversal() {
+		if (root == null) {
+			return;
+		}
+		// 生成一个队列 并且放回root
+		Queue<Node<E>> queue = new LinkedList<>();
+		// 往队列里,添加元素
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			// 从队头获取元素, 并且更新队列
+			Node<E> current = queue.poll();
+			System.out.println(current.element);
+			Node<E> left = current.left;
+			if (left != null) {
+				queue.add(left);
+			}
+			Node<E> right = current.right;
+			if (right != null) {
+				queue.add(right);
+			}
+		}
+
+	}
+
+	/**
+	 * 后序遍历
+	 */
+	public void postorderTraversal() {
+		postorderTraversal(root);
+	}
+
+	private void postorderTraversal(Node<E> node) {
+		if (node == null) {
+			return;
+		}
+		postorderTraversal(node.left);
+		postorderTraversal(node.right);
+		System.out.println(node.element);
+
+	}
+
+	private void postorderTraversal(Node<E> node, Visitor<E> visitor) {
+		System.out.println("postorder" + visitor.stop);
+		if (node == null || visitor.stop) {
+			return;
+		}
+		postorderTraversal(node.left);
+		postorderTraversal(node.right);
+		
+		if (visitor.stop) {
+			return;
+		}
+		visitor.stop = visitor.visitor(node.element);
+
+	}
+
+	/**
+	 * 后序遍历,
+	 * 
+	 * @param visitor 使用者
+	 */
+	public void postorder(Visitor<E> visitor) {
+		if (visitor == null) {
+			return;
+
+		}
+		postorderTraversal(root, visitor);
+	}
+
+	/**
+	 * 中序遍历
+	 */
+	public void inorderTraversal() {
+		inorderTraversal(root);
+
+	}
+
+	private void inorderTraversal(Node<E> node) {
+		if (node == null) {
+			return;
+		}
+		inorderTraversal(node.left);
+		System.out.println(node.element);
+		inorderTraversal(node.right);
+
+	}
+
+	private void inorderTraversal(Node<E> node, Visitor<E> visitor) {
+		System.out.println("inorder" + visitor.stop);
+		if (node == null || visitor.stop) {
+			return;
+		}
+		inorderTraversal(node.left);
+		if (visitor.stop) {
+			return;
+		}
+		visitor.stop = visitor.visitor(node.element);
+		inorderTraversal(node.right);
+
+	}
+
+	public void inorder(Visitor<E> visitor) {
+		if (visitor == null) {
+			return;
+		}
+		inorderTraversal(root, visitor);
+
+	}
+
+	/**
+	 * 先序遍历
+	 */
+	public void preorderTraversal() {
+		preorderTraversal(root);
+
+	}
+
+	/**
+	 * 前序遍历
+	 * 
+	 * @param parent
+	 */
+	private void preorderTraversal(Node<E> parent) {
+		// 递归结束的条件
+		if (parent == null) {
+			return;
+		}
+		System.out.println(parent.element);
+		preorderTraversal(parent.left);
+		preorderTraversal(parent.right);
+	}
+
+	private void preorderTraversal(Node<E> parent, Visitor<E> visitor) {
+		System.out.println("preorder" + visitor.stop);
+		// 递归结束的条件
+		if (parent == null || visitor.stop) {
+			return;
+		}
+
+		visitor.stop = visitor.visitor(parent.element);
+		preorderTraversal(parent.left);
+		preorderTraversal(parent.right);
+	}
+
+	public void preorder(Visitor<E> visitor) {
+		// 不用在递归的时候每次都判断了
+		if (visitor == null) {
+			return;
+		}
+		preorderTraversal(root, visitor);
+
+	}
+
 	/**
 	 * 添加元素
 	 * 
@@ -53,19 +251,32 @@ public class BinarySearchTree<E> {
 
 		// 找到父节点 从父节点 开始找到父节点
 		Node<E> node = root;
-		Node<E> parent = null;
+		// 默认的 父节点 从根节点开始查找
+		Node<E> parent = root;
+		// 记录要往哪个方向来添加
+		int cmp = 0;
 		while (node != null) {
-			int cmp = compare(element, node.element);
+			cmp = compare(element, node.element);
+			// 记录出来父节点
 			parent = node;
 			if (cmp > 0) {
 				// ======> 这个方向
 				node = node.right;
 			} else if (cmp < 0) {
-				node = node.right;
+				node = node.left;
 			} else {
+				// 如果两个值相等的话, 把新的值来覆盖旧的值
+				// 当前节点的真实数据 覆盖成新的值
+				node.element = element;
 				return;
 
 			}
+		}
+		Node<E> newNode = new Node<>(element, parent);
+		if (cmp > 0) {
+			parent.right = newNode;
+		} else {
+			parent.left = newNode;
 		}
 
 	}
@@ -78,8 +289,12 @@ public class BinarySearchTree<E> {
 	 * 
 	 */
 	private int compare(E e1, E e2) {
+		// 比較的方法
+		if (comparator != null) {
+			return comparator.compare(e1, e2);
 
-		return 0;
+		}
+		return ((Comparable<E>) e1).compareTo(e2);
 	}
 
 	/**
@@ -116,6 +331,14 @@ public class BinarySearchTree<E> {
 
 	}
 
+	// Visitor 访问者
+	public static abstract class Visitor<E> {
+		// 遍历是否结束的标记
+		boolean stop;
+
+		abstract boolean visitor(E element);
+	}
+
 	static class Node<E> {
 		Node<E> left;// 左边节点
 		Node<E> right; // 右边节点
@@ -127,6 +350,32 @@ public class BinarySearchTree<E> {
 			this.parent = parent;
 		}
 
+	}
+
+	@Override
+	public Object root() {
+		return root;
+	}
+
+	@Override
+	public Object left(Object node) {
+		Node<E> current = (Node<E>) node;
+		return current.left;
+	}
+
+	@Override
+	public Object right(Object node) {
+		Node<E> current = (Node<E>) node;
+		return current.right;
+	}
+
+	@Override
+	public Object string(Object node) {
+		Node<E> current = (Node<E>) node;
+		if (current.parent == null) {
+			return current.element + "_p(" + null + ")";
+		}
+		return current.element + "_p(" + current.parent.element + ")";
 	}
 
 }
--- 06二叉搜索树/src/com/mhz/tree/Main.java
+++ 06二叉搜索树/src/com/mhz/tree/Main.java
@@ -1,10 +1,93 @@
 package com.mhz.tree;
 
+import com.mhz.tree.BinarySearchTree.Visitor;
+import com.mhz.tree.printer.BinaryTrees;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static void testTree() {
+		Integer[] treeNodes = { 7, 4, 9, 2, 5, 8, 11, 3, 12, 1 };
+		
 
+		BinarySearchTree<Integer> trees = new BinarySearchTree<>();
+		for (int i = 0; i < treeNodes.length; i++) {
+			trees.add(treeNodes[i]);
+		}
+		BinaryTrees.print(trees);
+		System.out.println();
+
+//		trees.preorderTraversal();
+
+//		trees.inorderTraversal();
+//		trees.postorderTraversal();
+		// 7 ,4,9,2,5,8,11,1,3,12
+//		trees.levelOrderTraversal();|
+		
+		System.out.println("=====前序遍历=====");
+		trees.preorder(new Visitor<Integer>() {
+
+			@Override
+			boolean visitor(Integer element) {
+				System.out.print(element+"\t");
+				if (element.equals(new Integer(9)) ) {
+					System.out.println("==9");
+					return true;
+				}else {
+					return false;
+				}
+			
+			}
+		});
+		System.out.println("=====中序遍历=====");
+		trees.inorder(new Visitor<Integer>() {
+
+			@Override
+			boolean visitor(Integer element) {
+				System.out.print(element+"\t");
+				if (element == 9) {
+					return true;
+				}else {
+					return false;
+				}
+				
+				
+				
+			}
+		});;
+
+		System.out.println("=====后序遍历=====");
+		trees.postorder(new Visitor<Integer>() {
+
+			@Override
+			boolean visitor(Integer element) {
+				System.out.print(element+"\t");
+				if (element == 9) {
+					return true;
+				}else {
+					return false;
+				}
+				
+				
+			}
+		});;
+
+//		trees.levelOrder(new Visitor<Integer>() {
+//
+//			@Override
+//			public boolean visitor(Integer element) {
+//				System.out.println(element);
+//				if (element == 9) {
+//					return true;
+//				}
+//				return false;
+//
+//			}
+//		});
+
+	}
+
+	public static void main(String[] args) {
+		testTree();
 	}
 
 }
